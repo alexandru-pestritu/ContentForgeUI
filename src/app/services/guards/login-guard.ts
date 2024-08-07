a7dafc4b3adda680025a3874db5883ac214ff1c9
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-08-07 15:51:57 +0300

    Added login guard to prevent acces to login component if a user is logged in

---
+++ src/app/services/guards/login-guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | boolean {
+    return this.authService.isLoggedIn().pipe(
+      map(isLoggedIn => {
+        if (isLoggedIn) {
+        this.router.navigate(['/dashboard']);
+          return false;
+        }
+        return true;
+      })
+    );
+  }
+}

f7d83d77de9e2027d5a32bb6759a757e59d79a93
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-09-18 19:39:11 +0300

    DashboardStats and StockCheckLog models

---
+++ src/app/models/dashboard/dashboard-stats.ts
@@ -0,0 +1,7 @@
+export interface DashboardStats {
+    published_articles_count: number;
+    draft_articles_count: number; 
+    total_products_count: number;
+    out_of_stock_products_count: number;
+  }
+  
---
+++ src/app/models/stock-check-log/stock-check-log.ts
@@ -0,0 +1,8 @@
+export interface StockCheckLog {
+    id: number;
+    check_time: string;
+    duration: number;
+    in_stock_count: number;
+    out_of_stock_count: number;
+  }
+  

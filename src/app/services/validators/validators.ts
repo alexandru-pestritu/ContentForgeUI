e3d01ab476853da7ec1594b21cefb20d46c825af
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-10-19 22:14:25 +0300

    Changed email validation regex to accept , and - characters

--- src/app/services/validators/validators.ts
+++ src/app/services/validators/validators.ts
@@ -1,4 +1,4 @@
 export function isValidUrl(url: string): boolean {
-  const pattern = new RegExp("((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)");
+  const pattern = new RegExp("((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&/=,-]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&/=]*)");
   return pattern.test(url);
 }
